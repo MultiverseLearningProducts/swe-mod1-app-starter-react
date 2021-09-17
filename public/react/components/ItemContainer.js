a077a8113e8928f7ad55c565acf1a76f09441405
afreensafdar <afreen.leo502@gmail.com>
2021-09-17 13:40:04 -0500

    app.js coffee

--- public/react/components/ItemContainer.js
+++ public/react/components/ItemContainer.js
@@ -3,13 +3,13 @@ import { Item } from './Item';
 
 export const ItemContainer = (props) => {
 	const {onAdd} = props; // props.onAdd
-	const {onView}=props;
+	
 
 	return (
 		<div className="item-container">
 		{
 			props.items.map((item, idx) => {
-				return <Item onAdd={onAdd} onView={} item={item} key={idx} />;
+				return <Item onAdd={onAdd} item={item} key={idx} />;
 			
 			})
 
