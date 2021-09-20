e2c3902866d890fc0c1fd9941c4399fcb727bc05
RSalcido2019 <48775450+RSalcido2019@users.noreply.github.com>
2021-09-20 14:43:37 -0500

    hover

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 
+
 export const Item = (props) => {
   const { onAdd } = props;
 
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter=() => {
+      setIsHovering(true);
+  }
+
+  const handleMouseLeave=() => {
+    setIsHovering(false);
+}
+  
+ 
+
+
   return (
     <div className="card">
-      <img className="card-img" src={props.item.image} />
-      <h3 className="card-name">{props.item.name} </h3>
-      <h3 className="card-price">$ {props.item.price}</h3>
-      <button onClick={() => onAdd(props.item)}>Add To Cart</button>
-      <button onClick={() => onView(props.item)}>View Details</button>
+      
+        <img id={`${isHovering? "myImage": ""}`} onMouseLeave={ handleMouseLeave } onMouseEnter={ handleMouseEnter } className="card-img" src={props.item.image} />
+        <h3 className="card-name">{props.item.name} </h3>
+        <h3 className="card-price">$ {props.item.price}</h3>
+        <button onClick={() => onAdd(props.item)}>Add To Cart</button>
+        <button onClick={() => onView(props.item)}>View Details</button>
       
     </div>
   );
