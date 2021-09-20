5fe416cc635b274f8bb32dd29cb3df56e8de3ff0
afreensafdar <afreen.leo502@gmail.com>
2021-09-20 16:07:10 -0500

    single plant view

--- public/react/components/SingleItemContainer.js
+++ public/react/components/SingleItemContainer.js
@@ -1,10 +1,32 @@
-import React from 'react'
-
+import React, {useState,useEffect} from 'react'
+import {useParams} from "react-router-dom";
 
 export default function SingleItemContainer() {
+    const [singleItem ,setSingleItem]=useState({ });
+    let { id } = useParams();
+
+    async function fetchSingleProduct(id) {
+        try {
+        const response = await fetch(`http://localhost:3000/products/${id}`);
+        const responseJSON = await response.json();
+        console.log (responseJSON.product);
+    
+        setSingleItem(responseJSON.product);
+    
+        } catch (err) {
+        console.log("Oh no an error! ", err);
+        }
+    }
+
+    useEffect(() => {
+        fetchSingleProduct(id);
+    }, []);
+
     return (
         <div>
-            
+        <p> {singleItem.name}</p>
+        <p>{singleItem.image}</p>
+
         </div>
     )
 }
