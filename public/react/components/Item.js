3ed34c74095dd1715a50d50ea7d7ee25dda351e3
Judith Liem <25380136+jjliem@users.noreply.github.com>
2021-09-20 15:13:17 -0500

    add icon hover

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,31 +1,40 @@
 import React, { useState } from "react";
-
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export const Item = (props) => {
   const { onAdd } = props;
 
   const [isHovering, setIsHovering] = useState(false);
 
-  const handleMouseEnter=() => {
-      setIsHovering(true);
-  }
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
 
-  const handleMouseLeave=() => {
+  const handleMouseLeave = () => {
     setIsHovering(false);
-}
-  
- 
-
+  };
 
   return (
     <div className="card">
-      
-        <img id={`${isHovering? "myImage": ""}`} onMouseLeave={ handleMouseLeave } onMouseEnter={ handleMouseEnter } className="card-img" src={props.item.image} />
-        <h3 className="card-name">{props.item.name} </h3>
-        <h3 className="card-price">$ {props.item.price}</h3>
-        <button onClick={() => onAdd(props.item)}>Add To Cart</button>
-        <button onClick={() => onView(props.item)}>View Details</button>
-      
+      <div className="hover-container">
+        <div className="overlay">
+          <a href="#" class="icon" >
+            <ShoppingCartIcon></ShoppingCartIcon>
+          </a>
+        </div>
+        <img
+          id={`${isHovering ? "myImage" : ""}`}
+          onMouseLeave={handleMouseLeave}
+          onMouseEnter={handleMouseEnter}
+          className="card-img"
+          src={props.item.image}
+        />
+      </div>
+
+      <h3 className="card-name">{props.item.name} </h3>
+      <h3 className="card-price">$ {props.item.price}</h3>
+      <button onClick={() => onAdd(props.item)}>Add To Cart</button>
+      <button onClick={() => onView(props.item)}>View Details</button>
     </div>
   );
 };
