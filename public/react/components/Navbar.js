d14bef18ca5b1d17894f460de6beca65c91c1969
Judith Liem <25380136+jjliem@users.noreply.github.com>
2021-09-21 11:14:10 -0500

    add aria labels to Cart.js and Navbar.js

--- public/react/components/Navbar.js
+++ public/react/components/Navbar.js
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-left">
+      <div className="navbar-left" aria-label="shopping tabs">
         <h2 className="nav-item">
           <Link to="/plants">Plants</Link>
         </h2>
-        <h2 className="nav-item"> 
-        <Link to="/coffees">Coffee</Link>
+        <h2 className="nav-item">
+          <Link to="/coffees">Coffee</Link>
         </h2>
       </div>
-      <div className="navbar-center">
+      <div className="navbar-center" aria-label="home tab">
         <h2 className="nav-item">
           <a href="#">Plant Shed</a>
         </h2>
       </div>
-      <div className="navbar-right">
+      <div className="navbar-right" aria-label="cart tab">
         <h2 className="nav-item">
           <a href="#">Cart</a>
         </h2>
