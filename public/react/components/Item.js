bdc1c0093b01c1f563b29f63d3c22aa22b0b7beb
afreensafdar <afreen.leo502@gmail.com>
2021-09-21 10:20:57 -0500

    single plant css and lighthouse

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -35,7 +35,9 @@ export const Item = (props) => {
       <h3 className="card-name">{props.item.name} </h3>
       <h3 className="card-price">$ {props.item.price}</h3>
       <button onClick={() => onAdd(props.item)}>Add To Cart</button>
-      <Link to={`plant/${props.item.id}`}>View Details</Link>
+      <a className="btn" href="#"><button className="btn btn-info btnback">
+      <Link to={`plant/${props.item.id}`}>View Details</Link></button></a>
+  
     </div>
   );
 };
--- public/react/components/SingleItemContainer.js
+++ public/react/components/SingleItemContainer.js
@@ -1,9 +1,11 @@
 import React, {useState,useEffect} from 'react'
-import {useParams} from "react-router-dom";
-
-export default function SingleItemContainer() {
+import {useParams,Link} from "react-router-dom";
+import { Item } from './Item';
+import {Cart} from './Cart';
+export default function SingleItemContainer(props) {
     const [singleItem ,setSingleItem]=useState({ });
     let { id } = useParams();
+    const { onAdd } = props;
 
     async function fetchSingleProduct(id) {
         try {
@@ -22,11 +24,54 @@ export default function SingleItemContainer() {
         fetchSingleProduct(id);
     }, []);
 
-    return (
-        <div>
-        <p> {singleItem.name}</p>
-        <p>{singleItem.image}</p>
 
-        </div>
-    )
+   
+{/* <div classNameName="card">
+        <img classNameName="card-img"src={singleItem.image}/>
+        <div classNameName="row">
+            <div classNameName="col-2">{singleItem.name}</div>
+            <div classNameName="col-1 text-right">${singleItem.price}</div>
+          </div>
+        <h3 classNameName="card-name"> {singleItem.name} </h3>
+        <h3 classNameName="card-price">$ {singleItem.price}</h3>
+        <h3 classNameName="card-name">{singleItem.description} </h3>
+        <Link to="/plants">Back </Link>
+        <button onClick={() => onAdd(props.singleItem)} classNameName="add">
+            Add to Cart</button>
+    
+</div> */}
+
+return (
+    <div className="grid-singleItem">
+     <div className="container">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5"></div>
+            <div className="col mb-4">
+
+            <div className="card">
+    
+	
+      <div className="card-body">
+        <img src={singleItem.image} className="card-img-top singleItem-img"/><br/>
+        <ul>
+        <h2 className="card-title">{singleItem.name}</h2>
+        <h3>Price: ${singleItem.price} USD</h3>
+        <h3>Description: {singleItem.description}</h3>
+       
+        </ul><br/>
+     
+   
+<a className="btn" href="#"><button className="btn btn-info btnback">
+<Link to="/plants">Back </Link></button></a>
+
+  
+<a className="btn" href="#"><button className="btn btn-info btnback">
+<Link to="/plants">Add to Cart </Link></button></a>
+
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+ )
 }
