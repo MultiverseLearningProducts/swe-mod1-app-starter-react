057a8d7a1481287ac51c4038d88ed6b16387282c
Judith Liem <25380136+jjliem@users.noreply.github.com>
2021-09-23 11:58:00 -0500

    change checkout btn, add product banner

--- public/react/components/ItemContainer.js
+++ public/react/components/ItemContainer.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
-import { Item } from './Item';
+import React, { useState } from "react";
+import { Item } from "./Item";
+import banner from "./assets/banner.jpg";
 
 export const ItemContainer = (props) => {
   const { onAdd } = props; // props.onAdd
 
   return (
     <div className="item-container">
+      <div class="banner-img">
+        <div class="banner-text">
+          <h1>Products</h1>
+        </div>
+      </div>
       {props.items.map((item, idx) => {
         return <Item onAdd={onAdd} item={item} key={idx} />;
       })}
--- public/react/components/Navbar.js
+++ public/react/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CartSideBar from "./CartSideBar";
+import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
 
 export default function Navbar(props) {
   const { cartItems, onAdd, onRemove } = props;
