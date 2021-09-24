95663a135e7f46835c28b3e18c2b73457dffa9f9
afreensafdar <afreen.leo502@gmail.com>
2021-09-23 19:09:57 -0500

    form submit

--- public/react/components/PaymentForm.js
+++ public/react/components/PaymentForm.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import Button from '@mui/material/Button';
-import { Link } from "react-router-dom";
+import { validate } from "@babel/types";
 
 export default function PaymentForm() {
   const [input, setInput] = useState({
     fname: "",
-    lname: "",
     email: "",
     password: "",
     address: "",
     cardNumber: "",
     payment: "",
+
     
   });
+  const [nameError, setNameError] = useState(null)
 
   const [message, setMessage] = useState("");
 
@@ -24,43 +24,40 @@ export default function PaymentForm() {
     });
   }
 
+
   function handleSubmit(e) {
-    e.preventDefault(); // prevents refresh in all browsers
+    e.preventDefault(); 
+    // prevents refresh in all browsers
+    alert( `Payment Submitted ${input.fname} #${input.cardNumber}`)
+
     setMessage(
-      `Payment Submitted ${input.fname} ,${input.lname} ,#${input.cardNumber}`
+      `Payment Submitted ${input.fname} ,#${input.cardNumber}`
     );
   }
+
+  
 
   return (
     <div>
        {message ? ( message) : (
-          /*
+   
         <form onSubmit={handleSubmit}>
-        <h3>Payment Details Form</h3>
-          <div className="paymentContainer">
+         <h1 id="heading">Payment Checkout Form</h1>
+         <div className="paymentContainer">
           <div className="payment-center">
+
           <div>
-            <label for="fname">First Name: </label>
+            <label for="fname">Full Name: </label>
             <input
               type="text"
               id="fname"
               name="fname"
               value={input.fname}
               onChange={handleChange}
-              placeholder="Enter first name"
+              placeholder="Enter name" required
             />
-          </div>
+          </div> <br/>
 
-          <div>
-            <label for="lname">Last Name: </label>
-            <input
-              type="text"
-              name="lname"
-              value={input.lname}
-              onChange={handleChange}
-              placeholder="Enter last name"
-            />
-          </div>
           <div>
             <label for="email">Email: </label>
             <input
@@ -68,9 +65,9 @@ export default function PaymentForm() {
               name="email"
               value={input.email}
               onChange={handleChange}
-              placeholder="Enter email"
+              placeholder="Enter email"required
             />
-          </div>
+          </div> <br/>
 
           <div>
             <label for="password">Password: </label>
@@ -79,9 +76,9 @@ export default function PaymentForm() {
               name="password"
               value={input.password}
               onChange={handleChange}
-              placeholder="Enter password"
+              placeholder="Enter password" required
             />
-          </div>
+          </div> <br/>
 
             <div>
               <label for="address">Shipping Address: </label> 
@@ -91,129 +88,48 @@ export default function PaymentForm() {
              name="address"  
              value={input.address} 
              onChange={handleChange}
-             placeholder="Enter address">
+             placeholder="Enter address" required>
               </textarea>
-               <br/>
+             
                         
-         </div>
+         </div> <br/>
 
             <div>
-                <label for="cardNumber">Credit Card Number: </label> <br/>
+                <label for="cardNumber">Credit Card Number: </label> 
                 <input type="text"
                  name="cardNumber" 
                 value={input.card} 
                 onChange={handleChange}
-                placeholder="Enter card number" /> <br/>
-              </div>
+                placeholder="Enter card number" required /> 
+            </div> <br/>
 
 			<div>
 				<label for="payment">Payment Type: </label>
-                <select id="payment" name="payment" onChange={handleChange}>
-				<option></option>
+                <select id="payment" name="payment" placeholder="select payment" onChange={handleChange}>
+				<option>Select Type</option>
 				<option value="visa" >Visa</option>
 				<option value="amex">Amex</option>
 				<option vlaue="chase">Chase</option>
 				</select>
-			</div>
+			</div> <br/>
+
 
             <div>
 			    <button type="submit" className="btn">Submit Order</button>
-                <Button id="back-btn" component={Link} 
-               to={'/cart'}>Back </Button>
+                {/* <Button id="back-btn1" component={Link} 
+               to={'/cart'}>Back </Button> */}
             </div>
-            </div>
-            </div>
-        </form> */
-    // }
-
-    //      )}
-    //      </div>
-       
-    //      )
-    //  }
-
-
-     <h2>Payment Checkout Form</h2>
-<p>Resize the browser window to see the effect. When the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other.</p>
-<div class="row">
-  <div class="col-75">
-    <div class="container">
-      <form>
-      
-        <div class="row">
-          <div class="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="Enter name"  value={input.fname}
-              onChange={handleChange}>
-            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="Enter email" value={input.email}
-              onChange={handleChange}>
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="Enter address"  value={input.address}
-              onChange={handleChange}>
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="City"  value={input.city}
-              onChange={handleChange}>
-
-            <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="state" name="state" placeholder="NY"  value={input.state}
-              onChange={handleChange}>
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001"  value={input.zip}
-              onChange={handleChange}>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:red;"></i>
-              <i class="fa fa-cc-discover" style="color:orange;"></i>
-            </div>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="cname" name="cardname">
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber"  value={input.cardNumber}
-              onChange={handleChange}>
-            <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September"  value={input.expmonth}
-              onChange={handleChange}>
-            <div class="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018"  value={input.expyear}
-              onChange={handleChange}>
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="352"  value={input.cvv}
-              onChange={handleChange}>
-              </div>
-            </div>
-          </div>
-          
+            
         </div>
-        <label>
-          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
-        </label>
-        <input type="submit" value="Continue to checkout" class="btn">
-      </form>
     </div>
-  </div>
+</form> 
+
+
+        )}
+    </div>
+
+        )
+    }
 
 
 
-)}
-</div>
-
-)
-}
